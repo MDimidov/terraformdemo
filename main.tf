5fc65b246706340c8ee7663833c240cf02f02c16
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-08-12 17:39:58 +0300

    add storage account

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 3.52.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "StorageRG"
+    storage_account_name = "taskboardstoragemyro"
+    container_name = "taskboardstoragemyr"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
