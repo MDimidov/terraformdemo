7d409fd89f836505ac459354fb8b585208773696
Mariyan <116071995+MDimidov@users.noreply.github.com>
2024-08-12 17:41:00 +0300

    fix main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name = "StorageRG"
+    resource_group_name  = "StorageRG"
     storage_account_name = "taskboardstoragemyro"
-    container_name = "taskboardstoragemyr"
-    key = "terraform.tfstate"
+    container_name       = "taskboardstoragemyr"
+    key                  = "terraform.tfstate"
   }
 }
 
